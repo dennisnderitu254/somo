fff2b79e8bebffe775060398c17086fc602eb2fc
dennisnderitu254 <dknderitu@gmail.com>
2019-06-04 08:57:43 +0300

    adding instructor, course and video entities into the model

--- mysite/instructor/models.py
+++ mysite/instructor/models.py
@@ -4,20 +4,23 @@ class Instructor(models.Model):
 	firstname = models.CharField(max_length=50)
 	lastname = models.CharField(max_length=50)
 	title = models.CharField(max_length=50)
-	email = models.CharField(max_length=40)
-	instructorid = models.CharField(max_length=40)
+	email = models.EmailField(max_length=200)
+	instructorid = models.IntegerField(primary_key=True)
+	date_registered = models.DateField()
 
 class Course(models.Model):
 	coursetitle = models.CharField(max_length=50)
-	courseid = models.CharField(max_length=50)
+	courseid = models.IntegerField(primary_key=True)
+	description = models.CharField(max_length=250) 
+
 
 
 class Video(models.Model):
-	title = models.CharField(max_length=40)
-	description = models.CharField(max_length=50)
-	tags = models.CharField(max_length=40)
-	instructorid = models.CharField(max_length=50)
-	courseid = models.CharField(max_length=50)
+	title = models.CharField(max_length=50)
+	description = models.CharField(max_length=150)
+	tags = models.CharField(max_length=50)
+	instructorid = models.ForeignKey(Instructor)
+	courseid = models.ForeignKey(Course)
 
 
 
