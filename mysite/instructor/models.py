f964f2c3214e743e240436af3e3bee1fa8ab58a2
dennisnderitu254 <dknderitu@gmail.com>
2019-06-06 15:03:28 +0300

    adding description and internationalization to models

--- mysite/instructor/models.py
+++ mysite/instructor/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 class Instructor(models.Model):
+	DOCTOR = "Dr."
+	PROFESSOR = "Prof."
+	MR = "Mr."
+	MRS = "Mrs."
+	TITLE_CHOICES  = _([
+		(DOCTOR, "Doctor"),
+		(PROFESSOR, "Professor"),
+		(MR, "Mr."),
+		(MRS, "Mrs.")
+	])
 	firstname = models.CharField(max_length=50)
 	lastname = models.CharField(max_length=50)
-	title = models.CharField(max_length=50)
 	email = models.EmailField(max_length=200)
-	instructorid = models.IntegerField(primary_key=True)
+	title = _(models.CharField(choices=TITLE_CHOICES, description="This shows the salutation an instructor possesses"))
 	date_registered = models.DateField()
 
 class Course(models.Model):
