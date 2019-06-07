d89adc5a3087ffc5c725444aa4ccda5c35f4751c
dennisnderitu254 <dknderitu@gmail.com>
2019-06-07 14:51:35 +0300

    adding internationalization to titles

--- mysite/instructor/models.py
+++ mysite/instructor/models.py
@@ -6,31 +6,29 @@ class Instructor(models.Model):
 	PROFESSOR = "Prof."
 	MR = "Mr."
 	MRS = "Mrs."
-	TITLE_CHOICES  = _([
-		(DOCTOR, "Doctor"),
-		(PROFESSOR, "Professor"),
-		(MR, "Mr."),
-		(MRS, "Mrs.")
-	])
-	firstname = models.CharField(max_length=50)
-	lastname = models.CharField(max_length=50)
-	email = models.EmailField(max_length=200)
-	title = _(models.CharField(choices=TITLE_CHOICES, description="This shows the salutation an instructor possesses"))
+	TITLE_CHOICES  = [
+		(DOCTOR, _("Doctor")),
+		(PROFESSOR,_("Professor")),
+		(MR, _("Mr.")),
+		(MRS, _("Mrs.")),
+	]
+	firstname = models.CharField("instructor first name",max_length=50)
+	lastname = models.CharField("instructor last name",max_length=50)
+	email = models.EmailField("instructor email",max_length=200)
+	title = models.CharField("instructor email",choices=TITLE_CHOICES, help_text= _("This shows the salutation an instructor possesses"))
 	date_registered = models.DateField()
 
 class Course(models.Model):
-	coursetitle = models.CharField(max_length=50)
-	courseid = models.IntegerField(primary_key=True)
-	# description = models.CharField(max_length=250) 
-
+	coursetitle = models.CharField("course title",max_length=50)
+	courseid = models.IntegerField("course id", primary_key=True)
 
 
 class Video(models.Model):
-	title = models.CharField(max_length=50)
-	description = models.CharField(max_length=150)
-	tags = models.CharField(max_length=50)
-	instructorid = models.ForeignKey(Instructor, on_delete=models.CASCADE)
-	courseid = models.ForeignKey(Course, on_delete=models.CASCADE)
+	title = models.CharField("video title",max_length=50)
+	description = models.CharField("video description",max_length=150)
+	tags = models.CharField("video tags",max_length=50)
+	instructorid = models.ForeignKey("instructor id",Instructor, on_delete=models.CASCADE)
+	courseid = models.ForeignKey("course id",Course, on_delete=models.CASCADE)
 
 
 
