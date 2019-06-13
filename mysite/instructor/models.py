5ddb16c18c416e2fee06e7fef1c7e5248a97481a
dennisnderitu254 <dknderitu@gmail.com>
2019-06-13 15:22:59 +0300

    modified instructor and course model

--- mysite/instructor/models.py
+++ mysite/instructor/models.py
@@ -1,27 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
 
-class Instructor(models.Model):
+
+class Instructor(User):
 	DOCTOR = "Dr."
 	PROFESSOR = "Prof."
 	MR = "Mr."
 	MRS = "Mrs."
 	TITLE_CHOICES  = [
 		(DOCTOR, _("Doctor")),
-		(PROFESSOR,_("Professor")),
+		(PROFESSOR, _("Professor")),
 		(MR, _("Mr.")),
 		(MRS, _("Mrs.")),
 	]
-	firstname = models.CharField("instructor first name",max_length=50)
-	lastname = models.CharField("instructor last name",max_length=50)
-	email = models.EmailField("instructor email",max_length=200)
-	title = models.CharField("instructor email",choices=TITLE_CHOICES, help_text= _("This shows the salutation an instructor possesses"))
-	date_registered = models.DateField()
-
+	title = models.CharField(choices=TITLE_CHOICES, help_text= _("Instructor's salutation"))
+	
+	
 class Course(models.Model):
-	coursetitle = models.CharField("course title",max_length=50)
-	courseid = models.IntegerField("course id", primary_key=True)
-
+	name = models.CharField(_("name"), max_length=120)
+	instructor = models.ForeignKey(Instructor, verbose_name=_("instructor"), on_delete=models.CASCADE)
+	description = models.TextField(_("description"))
 
 class Video(models.Model):
 	title = models.CharField("video title",max_length=50)
